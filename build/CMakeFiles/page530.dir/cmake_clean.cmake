3ff5144ec38250aeaba4402bb1fb8c41ec3da7f5
AnatolyLysov <anatoly.lysov@gmail.com>
2025-07-12 17:20:01 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	new file:   build/.cmake/api/v1/query/client-vscode/query.json
    	new file:   build/.cmake/api/v1/reply/cache-v2-a50405d5c0f9298a8174.json
    	new file:   build/.cmake/api/v1/reply/cmakeFiles-v1-a87b84fb003d82fe7b92.json
    	new file:   build/.cmake/api/v1/reply/codemodel-v2-336a183ec9df3083b6d9.json
    	new file:   build/.cmake/api/v1/reply/directory-.-Debug-f5ebdc15457944623624.json
    	new file:   build/.cmake/api/v1/reply/index-2025-07-12T12-57-34-0801.json
    	new file:   build/.cmake/api/v1/reply/target-page530-Debug-4a0490cb2a4c6f442cbc.json
    	new file:   build/.cmake/api/v1/reply/toolchains-v1-46197055ddc1f38726a3.json
    	new file:   build/CMakeCache.txt
    	new file:   build/CMakeFiles/3.28.3/CMakeCCompiler.cmake
    	new file:   build/CMakeFiles/3.28.3/CMakeDetermineCompilerABI_C.bin
    	new file:   build/CMakeFiles/3.28.3/CMakeSystem.cmake
    	new file:   build/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c
    	new file:   build/CMakeFiles/3.28.3/CompilerIdC/a.out
    	new file:   build/CMakeFiles/CMakeConfigureLog.yaml
    	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake
    	new file:   build/CMakeFiles/Makefile.cmake
    	new file:   build/CMakeFiles/Makefile2
    	new file:   build/CMakeFiles/TargetDirectories.txt
    	new file:   build/CMakeFiles/cmake.check_cache
    	new file:   build/CMakeFiles/page530.dir/DependInfo.cmake
    	new file:   build/CMakeFiles/page530.dir/build.make
    	new file:   build/CMakeFiles/page530.dir/cmake_clean.cmake
    	new file:   build/CMakeFiles/page530.dir/compiler_depend.make
    	new file:   build/CMakeFiles/page530.dir/compiler_depend.ts
    	new file:   build/CMakeFiles/page530.dir/depend.make
    	new file:   build/CMakeFiles/page530.dir/flags.make
    	new file:   build/CMakeFiles/page530.dir/link.txt
    	new file:   build/CMakeFiles/page530.dir/progress.make
    	new file:   build/CMakeFiles/progress.marks
    	new file:   build/Makefile
    	new file:   build/cmake_install.cmake
    	new file:   build/compile_commands.json

---
+++ build/CMakeFiles/page530.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/page530.dir/page530.c.o"
+  "CMakeFiles/page530.dir/page530.c.o.d"
+  "page530"
+  "page530.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/page530.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
