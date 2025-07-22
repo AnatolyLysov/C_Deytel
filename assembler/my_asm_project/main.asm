c4c8e025f0c3aee0582dd87eda4d274fec69f033
AnatolyLysov <anatoly.lysov@gmail.com>
2025-07-22 18:12:41 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   compile_assembly_code_with_debug_information/main.s
    	new file:   main
    	new file:   main.asm
    	new file:   main.o
    	new file:   my/Enable Debuginfod
    	new file:   my/Exploring Stacks and Heaps using gdb-gef
    	new file:   my/compile_assembly_code_with_debug_information
    	new file:   my/dedug_line_number
    	new file:   my/diary
    	new file:   my/writing_and_debugging
    	new file:   ../writing_and_debugging
    	renamed:    ../../build/.cmake/api/v1/reply/index-2025-07-19T11-58-24-0001.json -> ../../build/.cmake/api/v1/reply/index-2025-07-22T13-36-14-0444.json
    	modified:   ../../diary
    	new file:   ../../info/gdb/gef/Exploring Stacks and Heaps using gdb-gef

---
+++ assembler/my_asm_project/main.asm
@@ -0,0 +1,18 @@
+section .data
+    hello db 'Hello, World!', 0
+
+section .text
+    global _start
+
+_start:
+    ; Write the string to stdout
+    mov rax, 1          ; syscall: sys_write
+    mov rdi, 1          ; file descriptor: stdout
+    mov rsi, hello      ; pointer to the string
+    mov rdx, 13         ; length of the string
+    syscall             ; invoke operating system to do the write
+
+    ; Exit the program
+    mov rax, 60         ; syscall: sys_exit
+    xor rdi, rdi        ; exit code 0
+    syscall             ; invoke operating system to exit
