c699eb5faa09fb14ea17df42c8eab66311f55e1b
AnatolyLysov <anatoly.lysov@gmail.com>
2025-07-24 16:46:22 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   compile_assembly_code_with_debug_information/main
    	new file:   compile_assembly_code_with_debug_information/main.o
    	modified:   compile_assembly_code_with_debug_information/main.s
    	new file:   my/The_Art_of_Debugging_with_GDB,_DDD,_and_Eclipse_Norman_Matloff_ru.pdf
    	new file:   my/compile_with_nasm_for_debug
    	modified:   my/diary
    	new file:   my/nasm.txt
    	new file:   my/nasm_64
    	new file:   my/nasm_commands

--- assembler/my_asm_project/compile_assembly_code_with_debug_information/main.s
+++ assembler/my_asm_project/compile_assembly_code_with_debug_information/main.s
@@ -1,18 +1,18 @@
 section .data
-    hello db 'Hello, World!', 0
+    msg db 'Hello, World!', 0  ; Null-terminated string
 
 section .text
     global _start
 
 _start:
-    ; Write the string to stdout
-    mov rax, 1          ; syscall: sys_write
-    mov rdi, 1          ; file descriptor: stdout
-    mov rsi, hello      ; pointer to the string
-    mov rdx, 13         ; length of the string
-    syscall             ; invoke operating system to do the write
+    ; Write the message to stdout
+    mov rax, 1          ; syscall number for sys_write
+    mov rdi, 1          ; file descriptor 1 is stdout
+    mov rsi, msg        ; pointer to the message
+    mov rdx, 13         ; message length
+    syscall             ; call kernel
 
     ; Exit the program
-    mov rax, 60         ; syscall: sys_exit
+    mov rax, 60         ; syscall number for sys_exit
     xor rdi, rdi        ; exit code 0
-    syscall             ; invoke operating system to exit
+    syscall             ; call kernel
